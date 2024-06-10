7e06ff5b8084b3f792b7cf80b1dcc0d734526b9b
William <willspri@gmail.com>
2024-06-10 17:29:26 +1200

    incorrect coordinates need to be fixed

--- locations.js
+++ locations.js
@@ -4,7 +4,17 @@ var videoLinks = [
     "https://www.youtube.com/watch?v=9L1jrC2-BTE", // 40.7460, 14.4989
     "https://www.youtube.com/watch?v=1HjK0BQXKYc", // 6.230833, -75.590553
     "https://www.youtube.com/watch?v=WUtB57qawoo", // 42.5195, -70.8967
-    "https://www.youtube.com/watch?v=nr3HUVWw0WI"  // 59.9139, 10.7522
+    "https://www.youtube.com/watch?v=nr3HUVWw0WI",  // 59.9139, 10.7522
+    "https://www.youtube.com/watch?v=2xGOPO65kWk", // 53.3455043282152, -6.2641804479797685
+    "https://www.youtube.com/watch?v=IYQ0i9kYXiM", // -37.80997907774387, 144.96279059264438
+    "https://www.youtube.com/watch?v=5gBqG5Jkugw", // 35.82928196031092, 129.21804083558212
+    "https://www.youtube.com/watch?v=YW48kYHzg2U", // 41.72120802992474, 44.798149620165226
+    "https://www.youtube.com/watch?v=7dfpryACUV4", //40.70803296569079, -73.99938685017098
+    "https://www.youtube.com/watch?v=LFOx-vmYrts", //46.02065756109587, 7.749054582030605
+    "https://www.youtube.com/watch?v=H9ynsH3T7vE", //42.50885709483389, 1.529559246029798
+    "https://www.youtube.com/watch?v=diNGaDsBt5c", //49.25460725593517, -123.06762140505892
+    "https://www.youtube.com/watch?v=-E7QZ0kz7lw", // 37.08631706060224, 25.149536919816473
+    "https://www.youtube.com/watch?v=bNVXk8HfYkE" //48.209404667545336, 16.373273010922173
 ];
 
 var coordinates = [
@@ -12,6 +22,15 @@ var coordinates = [
     [64.963051, -19.020835], // Iceland
     [40.7460, 14.4989], // Pompeii
     [6.230833, -75.590553], // Medellin
-    [42.5195, -70.8967], // Salem
-    [59.9139, 10.7522] // Oslo
+    [42.5195, -70.8967], // Salem, Massachussets, 3am
+    [59.9139, 10.7522], // Oslo
+    [53.3455043282152, -6.2641804479797685], //Dublin, Ireland (Temple Bar)
+    [-37.80997907774387, 144.96279059264438], //Melbourne CBD
+    [35.82928196031092, 129.21804083558212], // Sunset in Gyeongju, Korea and Woljeonggyo Bridge
+    [40.70803296569079, -73.99938685017098], //brooklyn Bridge, Brooklyn
+    [46.02065756109587, 7.749054582030605], //Zermatt Switzerland Coordinates incorrect
+    [42.50885709483389, 1.529559246029798], //Andorra la vella Coordinates incorrect
+    [49.25460725593517, -123.06762140505892], // East Vancouver, December 19, 20:22
+    [37.08631706060224, 25.149536919816473], // Parakia, Paros Port Windmill, Thurs, 15/07/2021, 11:16, 27C
+    [48.209404667545336, 16.373273010922173] // Vienna, Saint Stephen's Cathedral
 ];
