4b84e33cb4f03acc4a2bd975eb2229d0d911f7f1
William <willspri@gmail.com>
2024-06-10 16:26:03 +1200

    location data moved to separate file

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-var videoLinks = [
-    "https://www.youtube.com/watch?v=-Xlzej8RTTU", // 28.645166730940492, 77.21402962943293
-    "https://www.youtube.com/watch?v=7n2vqKHFO1Y", // 64.963051, -19.020835
-    "https://www.youtube.com/watch?v=9L1jrC2-BTE", // 40.7460, 14.4989
-    "https://www.youtube.com/watch?v=1HjK0BQXKYc", // 6.230833, -75.590553
-    "https://www.youtube.com/watch?v=WUtB57qawoo", // 42.5195, -70.8967
-    "https://www.youtube.com/watch?v=nr3HUVWw0WI"  // 59.9139, 10.7522
-];
-
-var coordinates = [
-    [28.645166730940492, 77.21402962943293], // Delhi
-    [64.963051, -19.020835], // Iceland
-    [40.7460, 14.4989], // Pompeii
-    [6.230833, -75.590553], // Medellin
-    [42.5195, -70.8967], // Salem
-    [59.9139, 10.7522] // Oslo
-];
-
 let currentCoordinate = null;
 let line = null;
 
